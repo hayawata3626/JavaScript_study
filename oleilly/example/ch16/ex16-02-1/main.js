0bd43807e4d3e092e3a873855b6c2550652477eb
早津渉 <hayatsu_wataru@MPC-1168.local>
2018-09-11 12:49:53 +0900

    16章完了

---
+++ oleilly/example/ch16/ex16-02-1/main.js
@@ -0,0 +1,47 @@
+console.log(Math.pow(2, 3));
+console.log(Math.pow(1.7, 2.3));
+console.log(Math.sqrt(16));
+console.log(Math.sqrt(15.5));
+console.log(Math.cbrt(27));
+console.log(Math.cbrt(22));
+console.log(Math.exp(1));
+console.log(Math.exp(5.5));
+console.log(Math.expm1(1));
+console.log(Math.expm1(5.5));
+console.log(Math.hypot(3, 4));
+console.log(Math.hypot(2, 3, 4));
+console.log(Math.abs(-5.5));
+console.log(Math.abs(5.5));
+console.log(Math.sin(-10.5));
+console.log(Math.sin(6.77));
+/* xの上限値 */
+console.log(Math.ceil(2.2));
+console.log(Math.ceil(-3.8));
+/* xの下限値 */
+console.log(Math.floor(2.8));
+console.log(Math.floor(-3.2));
+/* xの整数部分 */
+console.log(Math.trunc(7.7));
+console.log(Math.trunc(-5.8));
+/* xのをもっとも近い整数に四捨五入 */
+console.log(Math.round(7.2));
+console.log(Math.round(7.7));
+console.log(Math.round(-7.7));
+console.log(Math.round(-7.2));
+/* 最小の引数を返す*/
+console.log(Math.min(1, 2));
+console.log(Math.min(3, 0.5, 0.66));
+console.log(Math.min(3, 0.5, -0.66));
+/* 最大の引数を返す */
+console.log(Math.max(1, 2));
+console.log(Math.max(3, 0.5, 0.66));
+console.log(Math.max(-3, 0.5, -0.66));
+
+/* 三角関数 */
+console.log(Math.sin(Math.PI/2));
+console.log(Math.sin(Math.PI/4));
+console.log(Math.cos(Math.PI));
+console.log(Math.cos(Math.PI/4));
+
+
+
