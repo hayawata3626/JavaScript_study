80617fb6d4a8c4040272ac98e384189e7a70e0e0
早津渉 <hayatsu_wataru@MPC-1168.local>
2018-09-12 16:01:03 +0900

    18章終了

---
+++ oleilly/example/ch18/ex18-08-1/main.js
@@ -0,0 +1,20 @@
+function highlightParas(containing) {
+  if (typeof containing === "string") {
+    containing = new RegExp(`${containing}`);
+  }
+  const paras = document.getElementsByTagName('p');
+  for(let p of paras){
+    if(!containing.test(p.textContent)) continue;
+    p.classList.add('highlight');
+  }
+}
+
+function removeParaHighlights() {
+  const paras = document.getElementsByTagName('p');
+  for(let p of paras){
+    p.classList.remove("highlight");
+  }
+}
+
+const highlightActions = document.querySelectorAll('[data-action="highlight"]');
+console.log(highlightActions[0].dataset);
